763b875912e63058f3f52884a3c6baf80b4a82d1
Семён <semen13051992@mail.ru>
2025-12-25 23:43:00 +0300

    homework_3_update

--- homework/semen13051992/homework_3/task_2.py
+++ homework/semen13051992/homework_3/task_2.py
@@ -1,4 +1,4 @@
 number_1 = 6
 number_2 = 3
 
-print((number_1 - number_2)/(5 + number_1 * number_2))
+print((number_1 - number_2) / (5 + number_1 * number_2))
--- homework/semen13051992/homework_3/task_3.py
+++ homework/semen13051992/homework_3/task_3.py
@@ -5,4 +5,3 @@ number_2 = 3
 
 print(f"Среднее арифметическое: {(number_1 + number_2) / 2}")
 print(f"Среднее геометрическое: {math.sqrt(number_1 + number_2)}")
-
