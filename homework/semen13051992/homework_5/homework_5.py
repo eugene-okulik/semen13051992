02492b7ff881a4abe813f6708da5f3e751f62187
Семён <semen13051992@mail.ru>
2025-12-30 11:01:25 +0300

    'homework_5'

---
+++ homework/semen13051992/homework_5/homework_5.py
@@ -0,0 +1,36 @@
+person = ['John', 'Doe', 'New York', '+1372829383739', 'US']
+
+name, last_name, city, phone, country = person
+
+print(name)
+print(last_name)
+print(city)
+print(phone)
+print(country)
+
+a = 'результат операции: 42'
+b = 'результат операции: 514'
+c = 'результат работы программы: 9'
+
+text_1 = a.split()
+print(text_1)
+print(text_1[-1])
+
+text_2 = b.split()
+print(text_2)
+print(text_2[-1])
+
+text_3 = c.split()
+print(text_2)
+print(text_3[-1])
+
+print(int(text_1[-1]) + int(text_2[-1]) + int(text_3[-1]) + 10)
+
+students = ['Ivanov', 'Petrov', 'Sidorov']
+subjects = ['math', 'biology', 'geography']
+
+print('Student', ', ' .join(students), 'study these', 'subjects:', ', '.join(subjects))
+
+
+
+
