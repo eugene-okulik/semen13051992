996bf75f63ed74444aca85097e65bb17b8fadd27
Семён <semen13051992@mail.ru>
2025-12-30 11:06:21 +0300

    'homework_5_upgrade'

--- homework/semen13051992/homework_5/homework_5.py
+++ homework/semen13051992/homework_5/homework_5.py
@@ -30,7 +30,3 @@ students = ['Ivanov', 'Petrov', 'Sidorov']
 subjects = ['math', 'biology', 'geography']
 
 print('Student', ', ' .join(students), 'study these', 'subjects:', ', '.join(subjects))
-
-
-
-
