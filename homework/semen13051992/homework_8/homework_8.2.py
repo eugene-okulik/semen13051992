f40d5647c84d360796e43f402b11897785c0d557
Семён <semen13051992@mail.ru>
2026-01-08 18:29:44 +0300

    'homework_8'

---
+++ homework/semen13051992/homework_8/homework_8.2.py
@@ -0,0 +1,11 @@
+def fib(numbers):
+    a, b = 0, 1
+    for number in range(numbers):
+        yield a
+        a, b = b, a + b
+
+
+print(list(fib(6))[-1])
+print(list(fib(201))[-1])
+print(list(fib(1_001))[-1])
+print(list(fib(100_001))[-1])
