46dfff1cc48307933e62ee00764289e2643dc1ed
Семён <semen13051992@mail.ru>
2026-01-09 14:20:30 +0300

    'homework_8.2_upgrade_generator'

--- homework/semen13051992/homework_8/homework_8.2.py
+++ homework/semen13051992/homework_8/homework_8.2.py
@@ -1,11 +1,17 @@
-def fib(numbers):
+def fibonacci_generator(number):
     a, b = 0, 1
-    for number in range(numbers):
+    for i in range(number):
         yield a
         a, b = b, a + b
 
 
-print(list(fib(6))[5])
-print(list(fib(201))[200])
-print(list(fib(1_001))[1_000])
-print(list(fib(100_001))[100_000])
+numbers = [5, 200, 1_000, 100_000]
+
+fib = fibonacci_generator(10000000000000000000000000000000000)
+count = 1
+for n in numbers:
+    for num in fib:
+        if count == n:
+            print(num)
+            break
+        count += 1
