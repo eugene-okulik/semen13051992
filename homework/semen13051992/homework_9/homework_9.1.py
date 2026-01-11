0834d73fc9d2083178d7e7484df2e5f939a30ca1
Семён <semen13051992@mail.ru>
2026-01-11 19:15:14 +0300

    'homework_9_upgrade'

--- homework/semen13051992/homework_9/homework_9.1.py
+++ homework/semen13051992/homework_9/homework_9.1.py
@@ -2,7 +2,7 @@ from datetime import datetime
 
 data = "Jan 15, 2023 - 12:05:33"
 
-python_date = datetime.strptime(data,'%b %d, %Y - %H:%M:%S')
+python_date = datetime.strptime(data, '%b %d, %Y - %H:%M:%S')
 month = python_date.strftime('%B')
 new_data = python_date.strftime('%d.%m.%Y, %H:%M')
 
