7778c5f4aec117a1c8910a48fb356006810b0e79
Семён <semen13051992@mail.ru>
2026-01-19 22:28:11 +0300

    'hw_12_upgr'

--- homework/semen13051992/homework_12/hw_12.py
+++ homework/semen13051992/homework_12/hw_12.py
@@ -17,12 +17,12 @@ class Bouquet:
 
 class Lily(Bouquet):
     def __init__(self, name, color, stem_length, freshness, price, lifetime):
-        super().__init__( name, color, stem_length, freshness, price, lifetime)
+        super().__init__(name, color, stem_length, freshness, price, lifetime)
 
 
 class Violet(Bouquet):
     def __init__(self, name, color, stem_length, freshness, price, lifetime):
-        super().__init__( name, color, stem_length, freshness, price, lifetime)
+        super().__init__(name, color, stem_length, freshness, price, lifetime)
 
 
 class Tulip(Bouquet):
@@ -41,10 +41,10 @@ class Rose(Bouquet):
 
 
 flower_1 = Lily('Лилия', 'Белый', 50, 'fresh', 100, 5)
-flower_2 = Violet('Фиолент','Фиолетовый', 50, 'fresh', 80, 7)
-flower_3 = Tulip('Тюльпан','Красный', 50, 'fresh', 110, 6)
-flower_4 = Gladioli('Гладиолус','Синий', 50, 'fresh', 90, 8)
-flower_5 = Rose('Роза','Желтый', 50, 'fresh', 120, 7)
+flower_2 = Violet('Фиолент', 'Фиолетовый', 50, 'fresh', 80, 7)
+flower_3 = Tulip('Тюльпан', 'Красный', 50, 'fresh', 110, 6)
+flower_4 = Gladioli('Гладиолус', 'Синий', 50, 'fresh', 90, 8)
+flower_5 = Rose('Роза', 'Желтый', 50, 'fresh', 120, 7)
 
 bouquet = [flower_1, flower_2, flower_3, flower_4, flower_5]
 
@@ -83,11 +83,11 @@ class Bouquet:
 bouquets = Bouquet(bouquet)
 
 print(bouquet)
-print(f"Стоимость букета: {bouquets.bouquet_price()} р.")
+print(f"Стоимость букета: {bouquets.bouquet_price()}р.")
 print(f"Среднее время жизни цветов: {bouquets.avg_lifetime()} дней.")
 print(f"Цветы время жизни которых выше среднего {bouquets.avg_lifetime()} дней: {bouquets.above_avg()}")
-print(f"Средняя стоимость цветов : {bouquets.price_avg()}р")
-print(f"Цветы стоимость которых выше средней цены {bouquets.price_avg()}р : {bouquets.above_price_avg()}")
-print(f"Цветы стоимость которых ниже средней цены {bouquets.price_avg()}р : {bouquets.below_price_avg()}")
-print(f"Цветы цвет которых Белый : {bouquets.color_white()}")
-print(f"Цветы цвет которых Красный : {bouquets.color_red()}")
+print(f"Средняя стоимость цветов: {bouquets.price_avg()}р")
+print(f"Цветы стоимость которых выше средней цены {bouquets.price_avg()}р: {bouquets.above_price_avg()}")
+print(f"Цветы стоимость которых ниже средней цены {bouquets.price_avg()}р: {bouquets.below_price_avg()}")
+print(f"Цветы цвет которых Белый: {bouquets.color_white()}")
+print(f"Цветы цвет которых Красный: {bouquets.color_red()}")
