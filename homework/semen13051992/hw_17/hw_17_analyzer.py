e1e8163adfa502772c0d89f7ef35aa4c842402d5
Семён <semen13051992@mail.ru>
2026-02-15 12:38:38 +0300

    'hw_17_upg'

--- homework/semen13051992/hw_17/hw_17_analyzer.py
+++ homework/semen13051992/hw_17/hw_17_analyzer.py
@@ -9,12 +9,12 @@ args = parser.parse_args()
 
 for file in os.listdir(args.file):
     if file.endswith('.log'):
-        file_log = os.path.join(args.path, file)
+        file_log = os.path.join(args.file, file)
         with open(file_log, 'r') as log_file:
             for line in log_file:
                 line = line.strip()
                 date = re.findall(r'\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d{3}', line)
-                text = 'ERROR'
+                line = line.split()
                 if args.text in line:
                     index = line.index(args.text)
-                    print(date, line[index - 5: index + len(args.text) + 5])
+                    print(file, date, line[index - 5: index + len(args.text) + 5])
