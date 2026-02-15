31c514e41e869c1bbfa8d148a6d6a991c3dd13e5
Семён <semen13051992@mail.ru>
2026-02-15 14:54:11 +0300

    'hw_17_upg2'

--- homework/semen13051992/hw_17/hw_17_analyzer.py
+++ homework/semen13051992/hw_17/hw_17_analyzer.py
@@ -3,18 +3,22 @@ import argparse
 import re
 
 parser = argparse.ArgumentParser()
-parser.add_argument("file", help="File name")
+parser.add_argument("files", help="File name")
 parser.add_argument("-t", "--text", help="text for search")
 args = parser.parse_args()
 
-for file in os.listdir(args.file):
+for file in os.listdir(args.files):
     if file.endswith('.log'):
-        file_log = os.path.join(args.file, file)
+        file_log = os.path.join(args.files, file)
         with open(file_log, 'r') as log_file:
             for line in log_file:
-                line = line.strip()
                 date = re.findall(r'\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d{3}', line)
-                line = line.split()
                 if args.text in line:
-                    index = line.index(args.text)
-                    print(file, date, line[index - 5: index + len(args.text) + 5])
+                    line_split = line.split()
+                    index = line_split.index(args.text)
+                    stat_index = index - 5
+                    if stat_index < 0:
+                        stat_index = 0
+                    print(file)
+                    print(date)
+                    print(line_split[stat_index : index + 6])
