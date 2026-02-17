f44b5359a7cc44de896e54086d05c093f3a49154
Семён <semen13051992@mail.ru>
2026-02-18 00:19:44 +0300

    'hw_19_upg'

--- homework/semen13051992/hw_19/hw_19.py
+++ homework/semen13051992/hw_19/hw_19.py
@@ -6,23 +6,26 @@ def all_posts():
     print(response.json())
     print(response.status_code)
 
+
 all_posts()
 
+
 def one_post():
-    #post_id = new_post()
     response = requests.get('http://objapi.course.qa-practice.com/object/400')
     print(response.json())
     print(response.status_code)
 
+
 one_post()
+
 
 def new_post():
     body = {
-        "data":
-            {
-            "qcolor": "wblue",
-            "ssize": "fsmall"
-            },
+        "data" :
+                {
+                "qcolor": "wblue",
+                "ssize": "fsmall"
+                },
         "id": 1,
         "name": "crabbit"
     }
@@ -36,14 +39,15 @@ def new_post():
     print(response.status_code)
     return response.json()['id']
 
+
 def put_a_post():
     post_id = new_post()
     body = {
         "data":
-            {
-            "qcdgolor": "wbdglue",
-            "sdgsize": "fsmaldgl"
-            },
+                {
+                "qcdgolor": "wbdglue",
+                "sdgsize": "fsmaldgl"
+                },
         "id": 1,
         "name": "crabgdgdbit"
     }
@@ -56,6 +60,7 @@ def put_a_post():
     print(response.json())
     print(response.status_code)
     return response.json()['id']
+
 
 def patch_a_post():
     post_id = put_a_post()
@@ -72,10 +77,12 @@ def patch_a_post():
     print(response.status_code)
     return response.json()['id']
 
+
 def delete_a_post():
     post_id = patch_a_post()
     response = requests.delete(f'http://objapi.course.qa-practice.com/object/{post_id}')
     print(response)
     print(response.status_code)
+
 
 delete_a_post()
