9ed45258492595dbd06e1366db3107ea212cf773
Семён <semen13051992@mail.ru>
2026-02-22 12:47:35 +0300

    'test_hw20_pytest'

--- homework/semen13051992/hw_20_pytest/test_hw_20_pytest.py
+++ homework/semen13051992/hw_20_pytest/test_hw_20_pytest.py
@@ -10,14 +10,14 @@ def hello():
 
 
 @pytest.fixture()
-def test():
+def tests():
     print("before test")
     yield
     print("after test")
 
 
 @pytest.mark.critical
-def test_all_object(hello, test):
+def test_all_object(hello, tests):
     response = requests.get('http://objapi.course.qa-practice.com/object')
     print(response.json())
     print(response.status_code)
@@ -25,7 +25,7 @@ def test_all_object(hello, test):
 
 
 @pytest.mark.medium
-def test_one_post(test):
+def test_one_post(tests):
     response = requests.get('http://objapi.course.qa-practice.com/object/1')
     print(response.json())
     print(response.status_code)
@@ -52,11 +52,12 @@ def new_object():
     object_id = response.json()['id']
     print(object_id)
     yield object_id
-    print('deleting the post')
-    requests.delete(f'https://jsonplaceholder.typicode.com/posts/{object_id}')
+    response = requests.delete(f'http://objapi.course.qa-practice.com/object/{new_object}')
+    print(response)
+    print(response.status_code)
 
 
-def test_put_a_object(new_object, test):
+def test_put_a_object(new_object, tests):
     body = {
         "data":
         {
@@ -71,14 +72,12 @@ def test_put_a_object(new_object, test):
         json=body,
         headers=headers
     )
-
     print(response.json())
     print(response.status_code)
     assert response.status_code == 200
-    assert response.json()['id'] == str(new_object)
 
 
-def test_patch_a_object(new_object, test):
+def test_patch_a_object(new_object, tests):
     body = {
         "name": "pelgprtlpgr"
     }
@@ -91,10 +90,9 @@ def test_patch_a_object(new_object, test):
     print(response.json())
     print(response.status_code)
     assert response.status_code == 200
-    assert response.json()['id'] == new_object
 
 
-def test_delete_a_object(new_object, test):
+def test_delete_a_object(new_object, tests):
     response = requests.delete(f'http://objapi.course.qa-practice.com/object/{new_object}')
     print(response)
     print(response.status_code)
@@ -106,18 +104,22 @@ def test_delete_a_object(new_object, test):
     [
         ("red", "blue", "gold"),
         ("average", "big", "little"),
-        ("acrylic", "oil", "painting"),
-        ("yiutuieut", "uieurtu", "wyiuriuwr")
+        ("acrylic", "oil", "painting")
     ])
 def test_new_object(color, size, name):
-    body = {"data": {"color": color, "size": size}, "name": name}
+    body = {"data":
+                {"color": color,
+                 "size": size},
+            "name": name}
     headers = {'User-Agent': 'PostmanRuntime/7.51.1'}
     response = requests.post(
         'http://objapi.course.qa-practice.com/object',
         json=body,
         headers=headers
     )
+    assert response.status_code == 200
     object_id = response.json()['id']
     print(object_id)
     print('deleting the post')
-    requests.delete(f'https://jsonplaceholder.typicode.com/posts/{object_id}')
+    response = requests.delete(f'http://objapi.course.qa-practice.com/object/{object_id}')
+    assert response.status_code == 200
