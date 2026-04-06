c8a0ccf721c869d39be9ad9089b4006c0e64a1a8
Семён <semen13051992@mail.ru>
2026-04-06 23:41:41 +0300

    '1'

---
+++ homework/semen13051992/hw_25_selenium/1.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+text = 'cat'
+
+driver = webdriver.Chrome()
+driver.maximize_window()
+driver.get('https://www.qa-practice.com/elements/input/simple')
+search_input = driver.find_element(By.NAME, 'text_string')
+search_input.send_keys(text)
+search_input.send_keys(Keys.ENTER)
+result = driver.find_element(By.ID, 'result-text').text
+print(result)
