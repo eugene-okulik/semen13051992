3d970de68e0d70f872371f246eeb53fff05e3c30
Семён <semen13051992@mail.ru>
2026-04-07 17:49:50 +0300

    25_2

--- homework/semen13051992/hw_25_selenium/test_3_1.py
+++ homework/semen13051992/hw_25_selenium/test_3_1.py
@@ -5,6 +5,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
+
 @pytest.fixture()
 def driver():
     driver = webdriver.Chrome()
