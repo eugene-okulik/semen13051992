6ab0343e4a61ac3a41d693d0f503a0d7f7de16cb
Семён <semen13051992@mail.ru>
2026-04-08 18:24:42 +0300

    25.2_upg

--- homework/semen13051992/hw_25_selenium/25.2.py
+++ homework/semen13051992/hw_25_selenium/25.2.py
@@ -2,12 +2,16 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+import time
 
 firstname = 'Ivan'
 lastname = 'Smit'
 email = 'IvanSmith@mail.ru'
 mobile = '375257359284'
-subjects = 'qa-practice'
 
 driver = webdriver.Chrome()
 driver.maximize_window()
@@ -31,24 +35,27 @@ mobile_input.send_keys(mobile)
 data_input = driver.find_element(By.ID, 'dateOfBirthInput')
 data_input.click()
 
-months = driver.find_elements(By.CLASS_NAME, 'react-datepicker__month-select')
+months = driver.find_elements(By.TAG_NAME, 'option')
 for month in months:
-    if month.text == 'April':
+    if month.text == "January":
         month.click()
 
-years = driver.find_elements(By.CLASS_NAME, 'react-datepicker__year-select')
+years = driver.find_elements(By.TAG_NAME, 'option')
 for year in years:
-    if year.text == '2026':
+    if year.text == "2020":
         year.click()
 
-days = driver.find_elements(By.CLASS_NAME, 'react-datepicker__day')
-for day in days:
-    if day.text == 5:
-        day.click()
+days = driver.find_element(By.CSS_SELECTOR, '.react-datepicker__day.react-datepicker__day--013')
+days.click()
 
-subject = driver.find_element(By.ID, 'subjectsInput')
-subject.send_keys(subjects)
-
+subject_input = driver.find_element(By.ID, 'subjectsInput')
+subject_input.click()
+subject_input.send_keys('a')
+subject_input.send_keys(Keys.ARROW_DOWN)
+subject_input.send_keys(Keys.ENTER)
+subject_input.send_keys('c')
+subject_input.send_keys(Keys.ARROW_DOWN)
+subject_input.send_keys(Keys.ENTER)
 
 hobbi1 = driver.find_element(By.ID, 'hobbies-checkbox-1')
 hobbi1.click()
@@ -56,21 +63,18 @@ hobbi1.click()
 hobbi3 = driver.find_element(By.ID, 'hobbies-checkbox-3')
 hobbi3.click()
 
-driver.execute_script("window.scrollBy(0,10000)")
+driver.find_element(By.ID, 'submit').send_keys(Keys.TAB)
+driver.find_element(By.ID, 'submit').send_keys(Keys.TAB)
 
 adsress = driver.find_element(By.ID, 'currentAddress')
 adsress.send_keys('belarus')
 
 state = driver.find_element(By.ID, 'react-select-3-input')
-state.click()
-state.send_keys(Keys.ARROW_DOWN)
 state.send_keys(Keys.ARROW_DOWN)
 state.send_keys(Keys.ENTER)
 
-city = driver.find_element(By.ID, 'react-select-4-input')
-city.click()
-state.send_keys(Keys.ARROW_DOWN)
-state.send_keys(Keys.ENTER)
+city = driver.find_element(By.ID, 'react-select-4-live-region')
+state.send_keys('Noida')
 
 submit = driver.find_element(By.ID, 'submit')
 submit.click()
