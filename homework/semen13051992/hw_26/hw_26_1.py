0107347d1f62aa29658cf4897d117a0bea56f1bc
Семён <semen13051992@mail.ru>
2026-04-19 13:11:25 +0300

    hw_26_upg

--- homework/semen13051992/hw_26/hw_26_1.py
+++ homework/semen13051992/hw_26/hw_26_1.py
@@ -3,7 +3,7 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
+
 from selenium.webdriver.common.action_chains import ActionChains
 
 from selenium.webdriver.support.wait import WebDriverWait
